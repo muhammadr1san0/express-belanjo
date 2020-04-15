3a5288b9e6e7c5f474b2f3071e292528fce5dd51
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2020-04-15 22:25:25 +0700

    add file

---
+++ src/configs/db.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: process.env.HOST_DB,
+  user: process.env.USER_DB,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DATABASE
+
+})
---
+++ src/controller/User.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken')
+const MiscHelper = require('../helpers/Helper');
+
+module.exports = {
+  login: (req, res) => {
+    const {handphone, password} = req.body;
+  }
+}
